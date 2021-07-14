de7176e13fb6ea57220fd1414c2c171e66bcd319
Rafael Oviedo <oviedo.rp@gmail.com>
2021-07-14 20:55:21 -0300

    searchBar comp done, working on weather comp

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,59 @@
 <template>
-    <form class="formContainer">
-        <input type="text" class="textBox" />
+    <form
+        class="formContainer"
+        @submit.prevent="searchCity(inputValue.cityInput)"
+    >
+        <input
+            type="text"
+            class="textBox"
+            placeholder="Type your city here..."
+            v-model="inputValue.cityInput"
+        />
         <input type="submit" name="submit" class="submitButton" />
     </form>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "SearchBar",
+    data() {
+        return {
+            inputValue: {
+                cityInput: "",
+            },
+        };
+    },
+    methods: {
+        async searchCity(citySearched) {
+            let cityFetched = await axios(
+                `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=b962bea41d7b3b0eb8d8dfb7906e2978&units=metric`
+            );
+            let cityData = cityFetched.data;
+            console.log(cityData);
+            if (cityData.main !== undefined) {
+                let city = {
+                    min: Math.round(cityData.main.temp_min),
+                    max: Math.round(cityData.main.temp_max),
+                    img: cityData.weather[0].icon,
+                    id: cityData.id,
+                    wind: cityData.wind.speed,
+                    temp: cityData.main.temp,
+                    name: cityData.name,
+                    weather: cityData.weather[0].main,
+                    clouds: cityData.clouds.all,
+                    latitude: cityData.coord.lat,
+                    length: cityData.coord.lon,
+                };
+                console.log(city);
+                this.$emit("city-submitted", city);
+            } else {
+                alert("City not found");
+            }
+            this.inputValue.cityInput = "";
+        },
+    },
 };
 </script>
 
@@ -23,6 +69,7 @@ export default {
     border-radius: 20px 0 0 20px;
     border: 2px solid grey;
     border-right: none;
+    outline: none;
 }
 
 .submitButton {
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-        <search-bar class="searchBar" />
+        <search-bar class="searchBar" @city-submitted="addCity" />
         <h1 class="weatherTitle">To find a city, type the name above</h1>
+        <ul class="cardsContainer">
+            <li v-for="(city, id) in cities" :key="id" class="cityCard">
+                <button class="cardXbutton">&times;</button>
+                <p class="cardName">{{ city.name }}</p>
+                <p class="cardMinTemp">Min temp: {{ city.min }}</p>
+                <p class="cardMaxTemp">Max temp: {{ city.max }}</p>
+                <img
+                    class="weatherIcon"
+                    src="http://openweathermap.org/img/wn/{{city.img}}@2x.png"
+                />
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,6 +23,16 @@ import SearchBar from "./SearchBar.vue";
 export default {
     components: { SearchBar },
     name: "Weather",
+    data() {
+        return {
+            cities: [],
+        };
+    },
+    methods: {
+        addCity(city) {
+            this.cities.push(city);
+        },
+    },
 };
 </script>
 
@@ -27,4 +49,65 @@ export default {
     top: 12%;
     left: 38%;
 }
+
+.cardsContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    position: absolute;
+    top: 20%;
+    left: 0;
+    width: 97.2%;
+    height: 70vh;
+}
+
+.cityCard {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas:
+        "button button button button"
+        "title  title  title  title"
+        "min    min    max    max"
+        "image   image   image   image";
+    list-style-type: none;
+    margin-top: 2%;
+    width: 20%;
+    height: 30%;
+    border: 2px solid black;
+    border-radius: 10px;
+}
+
+.cardXbutton {
+    transform: translate(702%);
+    grid-area: button;
+    width: 12.5%;
+    height: 100%;
+    border-radius: 0 10px 0 0;
+    border: none;
+    cursor: pointer;
+}
+
+.cardName {
+    grid-area: title;
+}
+
+.cardMinTemp {
+    grid-area: min;
+}
+
+.cardMaxTemp {
+    grid-area: max;
+}
+
+.weatherIcon {
+    transform: translate(50%);
+    grid-area: image;
+}
 </style>
+
+display: grid; grid-template-columns: 1fr 1fr 1fr 1fr; grid-template-rows: 1fr
+1fr 1fr 1fr 1fr; grid-template-areas: "button button button button" "title title
+title title" "props1 props1 props2 props2" "info1 info1 info2 info2" "image
+image image image"; background: white; width: 320px; height: 230px;
+border-radius: 15px; margin-top: 70px; position: relative; top: 15%; }
