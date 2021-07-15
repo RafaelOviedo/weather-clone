8f1855cf5460a4eb4aed856fdb3c80ef47953276
Rafael Oviedo <oviedo.rp@gmail.com>
2021-07-15 15:21:36 -0300

    project done. To review the routing error

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,45 @@
 <template>
-    <div class="aboutComponent">
-        <p class="aboutTitle">About component</p>
+    <div class="aboutContainer">
+        <p class="aboutTitle">About</p>
+        <p class="aboutText">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Repellendus qui, eveniet sunt, rerum ea porro soluta aperiam maiores
+            deleniti unde aut ex voluptatum velit culpa optio laudantium
+            similique commodi incidunt? Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. <br />
+            Repellendus qui, eveniet sunt, rerum ea porro soluta aperiam maiores
+            deleniti unde aut ex voluptatum velit culpa optio laudantium
+            similique commodi incidunt?
+        </p>
+        <ul class="socialMediaBox">
+            <li>
+                <a target="_blank" href="https://codepen.io/dgtgnd">
+                    <img
+                        class="codepenLogo"
+                        src="https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png"
+                        alt="codepen logo"
+                    />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://github.com/RafaelOviedo">
+                    <img
+                        class="githubLogo"
+                        src="https://pngimg.com/uploads/github/github_PNG58.png"
+                        alt="github logo"
+                    />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.instagram.com/dgtgnd/">
+                    <img
+                        class="instagramLogo"
+                        src="https://img.icons8.com/ios-glyphs/452/instagram-circle.png"
+                        alt="instagram logo"
+                    />
+                </a>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,9 +50,52 @@ export default {
 </script>
 
 <style scoped>
-.aboutTitle {
+.aboutContainer {
     position: absolute;
-    top: 20%;
-    left: 40%;
+    left: -1%;
+    display: flex;
+    flex-direction: column;
+    width: 101%;
+    height: 92vh;
+    font-size: 30px;
+    background: black;
+    color: white;
+    overflow: hidden;
+}
+
+.aboutTitle {
+    font-size: 70px;
+    font-weight: bold;
+    position: absolute;
+    top: 8%;
+    left: 20%;
+}
+
+.aboutText {
+    font-size: 20px;
+    position: absolute;
+    top: 30%;
+    left: 7%;
+    margin-right: 10%;
+}
+
+.socialMediaBox {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    list-style-type: none;
+    position: absolute;
+    top: 70%;
+    left: 24%;
+}
+
+.githubLogo,
+.codepenLogo,
+.instagramLogo {
+    background: white;
+    border-radius: 50%;
+    width: 90px;
+    height: 90px;
 }
 </style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,16 +2,39 @@
     <div>
         <search-bar class="searchBar" @city-submitted="addCity" />
         <h1 class="weatherTitle">To find a city, type the name above</h1>
-        <card-city :cities="cities"></card-city>
+        <ul class="cardsContainer">
+            <li v-for="(city, id) in cities" :key="id" class="cityCard">
+                <button class="cardXbutton" @click="removeCard(city.id)">
+                    &times;
+                </button>
+                <p class="cardName">{{ city.name }}</p>
+                <p class="cardMinTemp">Min temp: {{ city.min }}</p>
+                <p class="cardMaxTemp">Max temp: {{ city.max }}</p>
+                <img
+                    class="weatherIcon"
+                    src="http://openweathermap.org/img/wn/{{city.img}}@2x.png"
+                />
+                <br />
+                <p class="aditionalInfo">
+                    <strong>Aditional city info</strong>
+                </p>
+                <br />
+                <p class="cardTemp">Temperature: {{ city.temp }}</p>
+                <p class="cardWeather">Weather: {{ city.weather }}</p>
+                <p class="cardWind">Wind: {{ city.wind }}</p>
+                <p class="cardClouds">Clouds: {{ city.clouds }}</p>
+                <p class="cardLatitude">Latitude: {{ city.latitude }}</p>
+                <p class="cardLength">Length: {{ city.length }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 import SearchBar from "./SearchBar.vue";
-import CardCity from "./CardCity.vue";
 
 export default {
-    components: { SearchBar, CardCity },
+    components: { SearchBar },
     name: "Weather",
     data() {
         return {
@@ -21,6 +44,9 @@ export default {
     methods: {
         addCity(city) {
             this.cities.push(city);
+        },
+        removeCard(id) {
+            this.cities = this.cities.filter((city) => city.id !== id);
         },
     },
 };
@@ -38,5 +64,90 @@ export default {
     position: absolute;
     top: 12%;
     left: 38%;
+}
+
+.cardsContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    position: absolute;
+    top: 20%;
+    left: 0%;
+    width: 97.2%;
+    height: 70vh;
+}
+
+.cityCard {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas:
+        "button button button button"
+        "title  title  title  title"
+        "min    min    max    max"
+        "image   image   image   image"
+        "info    info    info    info"
+        "temp    temp    weath   weath"
+        "wind    wind    clouds  clouds"
+        "lat     lat     length  length";
+    list-style-type: none;
+    margin-top: 2%;
+    margin-right: 5%;
+    width: 20%;
+    height: 65%;
+    border: 2px solid black;
+    border-radius: 10px;
+}
+
+.cardXbutton {
+    transform: translate(569%, -2%);
+    grid-area: button;
+    width: 15%;
+    height: 120%;
+    border-radius: 0 10px 0 5px;
+    border: none;
+    cursor: pointer;
+    background: red;
+    color: white;
+}
+
+.cardName {
+    grid-area: title;
+}
+
+.cardMinTemp {
+    grid-area: min;
+}
+
+.cardMaxTemp {
+    grid-area: max;
+}
+
+.weatherIcon {
+    transform: translate(47%);
+    grid-area: image;
+}
+
+.aditionalInfo {
+    grid-area: info;
+}
+
+.cardTemp {
+    grid-area: temp;
+}
+.cardWeather {
+    grid-area: weath;
+}
+.cardWind {
+    grid-area: wind;
+}
+.cardClouds {
+    grid-area: clouds;
+}
+.cardLatitude {
+    grid-area: lat;
+}
+.cardLength {
+    grid-area: length;
 }
 </style>
