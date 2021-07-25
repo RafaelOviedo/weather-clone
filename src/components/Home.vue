3fcd02916e4cb2008159f5fe5d52810e01852d7e
Rafael Oviedo <oviedo.rp@gmail.com>
2021-07-25 12:15:01 -0300

    changing home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,7 @@
 <template>
     <div class="homeContainer">
-        <h1 class="homeTitle">My weather app clone</h1>
+        <h1 class="homeTitle">Weather app clone</h1>
         <p class="homeSubtitle">To see the weather go to the Weather Section</p>
-        <img
-            src="https://cdn.wallpapersafari.com/96/48/VKkC4r.jpg"
-            alt=""
-            class="homeImage"
-        />
     </div>
 </template>
 
@@ -25,18 +20,12 @@ export default {
     height: 90vh;
 }
 
-.homeImage {
-    width: 100%;
-    height: 90vh;
-    filter: brightness(60%);
-}
-
 .homeTitle {
     position: absolute;
     top: 25%;
     left: 20%;
     z-index: 5;
-    color: white;
+    color: black;
     font-size: xx-large;
 }
 
@@ -45,7 +34,7 @@ export default {
     top: 30%;
     left: 17%;
     z-index: 5;
-    color: white;
+    color: black;
     font-size: xx-large;
 }
 </style>
